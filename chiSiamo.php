f6f05cbe921a089f9052c3ddd3c6e30522988c44
s275276@studenti.polito.it <s275276@studenti.polito.it>
2024-09-06 16:40:13 +0200

    luca

---
+++ chiSiamo.php
@@ -0,0 +1,27 @@
+<?php require 'index.php'; ?>
+
+
+<script>
+setTitolo('Chi siamo');
+
+</script>
+
+<body>
+
+  <div class=" container mt-5 ">
+    <div class="row justify-content-center">
+      <div class="col-md-8 transparent-bg"><div class="nomeForm">CHI SIAMO</div>
+        
+          <div class="row justify-content-center">
+              <p>FARM ECO BREEZE è un'idea nata da un team di studenti appassionati, che crede nel futuro dell'agricoltura sostenibile e nel benessere animale. </p>
+              <p>Il nostro progetto si focalizza sulla modernizzazione della stalla, combinando tradizione e innovazione per creare un ambiente dove l'animale è al centro.</p>
+          </div>
+          
+      </div>
+  </div>
+
+
+
+</body>
+
+<?php require 'footer.html'; ?>
