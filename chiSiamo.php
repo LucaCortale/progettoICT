df0d7276ec2b05e0a458befdc02c9414f8504539
s275276@studenti.polito.it <s275276@studenti.polito.it>
2024-09-07 19:21:31 +0200

    luca

--- chiSiamo.php
+++ chiSiamo.php
@@ -10,14 +10,55 @@ setTitolo('Chi siamo');
 
   <div class=" container mt-5 ">
     <div class="row justify-content-center">
-      <div class="col-md-8 transparent-bg"><div class="nomeForm">CHI SIAMO</div>
-        
-          <div class="row justify-content-center">
-              <p>FARM ECO BREEZE è un'idea nata da un team di studenti appassionati, che crede nel futuro dell'agricoltura sostenibile e nel benessere animale. </p>
-              <p>Il nostro progetto si focalizza sulla modernizzazione della stalla, combinando tradizione e innovazione per creare un ambiente dove l'animale è al centro.</p>
-          </div>
+      <div class="col-md-8 transparent-bg-chiSiamo"><div class="nomeForm">CHI SIAMO</div>
+        <div class="row justify-content-center scritte2" style="text-align: center;">
+
+            <p>FARM ECO BREEZE è un'idea nata da un team di studenti appassionati, che crede nel futuro dell'agricoltura sostenibile e nel benessere animale. </p>
+            <div><p class="nomeForm">IL NOSTRO TEAM</p>
+            <div class="team-row">
+              <div class="employee">
+                  <img style='width: 100px; height: auto;'  src="image/cortaleLuca.jpg" alt="Foto ">
+                  <div class="employee-info">
+                      <h2>Nicolo Antonio Fiore</h2>
+                      <h3>Developer</h3>
+                  </div>
+                </div>
+              <div class="employee">
+                  <img style='width: 100px; height: auto;'  src="image/cortaleLuca.jpg" alt="Foto ">
+                  <div class="employee-info">
+                      <h2>Lorenzo Canini</h2>
+                      <h3>Developer</h3>
+                  </div>
+                </div>
+            </div>
+            <div class="team-row">
+              <div class="employee">
+                <img style='width: 100px; height: auto;'  src="image/cortaleLuca.jpg" alt="Foto ">
+                <div class="employee-info">
+                    <h2>Luca Cortale</h2>
+                    <h3>Developer</h3>
+                    <p> Sviluppatore con esperienza nella programmazione full-stack, dedito a realizzare applicazioni performanti e scalabili. Appassionato di innovazione e tecnologia.
+                    <a href="https://www.linkedin.com/" target="_blank">
+                            <img class="logoSocial"style='width: 30px; height: auto;' src="image/linkedin.png" alt="Linkedin Logo">
+                        </a>
+                    </p>
+                </div>
+              </div>
+              <div class="employee">
+                <img style='width: 100px; height: auto;'  src="image/cortaleLuca.jpg" alt="Foto ">
+                <div class="employee-info">
+                    <h2>Alex Chiappero </h2>
+                    <h3>Developer</h3>
+                    <p> </p>
+                </div>
+              </div>
+            </div>
+            </div>
+        </div>
           
-      </div>
+       </div>
+          
+    </div>
   </div>
 
 
