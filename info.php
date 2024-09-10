b46631ea12ee44c717e6c67fd47151e57fce613f
Lorenzo Canini <lorenzo.canini@libero.it>
2024-09-10 12:56:39 +0200

    Fix paragrafo info

--- info.php
+++ info.php
@@ -16,7 +16,7 @@ setTitolo('Info');
               <p>Benvenuti  nel progetto di modernizzazione della stalla, dove l'innovazione incontra la tradizione.
               Il nostro obiettivo è trasformare gli ambienti rurali, migliorando la vita degli animali e ottimizzando la produttività attraverso soluzioni moderne e sostenibili.</p>
               <p>Al cuore del nostro progetto c'è il benessere degli animali. 
-              Ogni dettaglio della nostra stalla è stato progettato pensando alle esigenze degli animali, dalla ventilazione ottimale all'acqua nebulizzata per fornire conforto agli animali nei giorni di caldo più intenso<./p>
+              Ogni dettaglio della nostra stalla è stato progettato pensando alle esigenze degli animali, dalla ventilazione ottimale all'acqua nebulizzata per fornire conforto agli animali nei giorni di caldo più intenso.</p>
               <p>Sfruttiamo tecnologie all'avanguardia come sensori intelligenti e automazioni per monitorare il benessere degli animali e rendere il processo di gestione più efficiente ed ecologico.</p>
               <p>Il rispetto dell'ambiente è fondamentale per noi, infatti tramite le nostre soluzioni si riducono gli sprechi, attivando e disattivando ventole e nebulizzatori solo quando necessario, portando
                 un risparmio non indifferente.</p>
